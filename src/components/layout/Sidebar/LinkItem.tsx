a4611a2cdbd7c00e5e4a554bd8eacb2ca0ebb81a
Irwanto Huang <irwanto.ng11@gmail.com>
2024-08-27 21:05:18 +0700

    add find active path

--- src/components/layout/Sidebar/LinkItem.tsx
+++ src/components/layout/Sidebar/LinkItem.tsx
@@ -1,5 +1,5 @@
 import { ElementType } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 interface LinkItemProps {
     title: string,
@@ -9,8 +9,9 @@ interface LinkItemProps {
 }
 
 const LinkItem = ({ title, url, Icon, mobileView }: LinkItemProps) => {
+    const path = useLocation();
     return (
-        <Link to={url} className={`flex transition-colors hover:bg-neutral-100 duration-100 text-sm rounded whitespace-nowrap text-ellipsis ${mobileView ? 'gap-1 text-[10px] font-light py-2 flex-col items-center' : 'gap-4 flex-row px-2 py-2 items-center'}`}>
+        <Link to={url} className={`flex transition-colors hover:bg-neutral-100 duration-100 text-sm rounded whitespace-nowrap text-ellipsis ${mobileView ? 'gap-1 text-[10px] font-light py-2 flex-col items-center' : 'gap-4 flex-row px-2 py-3 items-center'} ${path.pathname === url ? 'bg-secondary hover:bg-secondary-hover' : ''}`}>
             <Icon strokeWidth={1} className="w-4 h-4 lg:w-5 lg:h-5" />
             { title }
         </Link>
